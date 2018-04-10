d01d4474e32c3731192da52ce07c892e3261e5e4
LI SHENG <li.sheng0305@gmail.com>
2018-04-10 14:30:51 +1000

    Initial implementation - in progress

---
+++ server.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 3000;
+
+var healthcheck = require("express-healthcheck");
+
+var metadata = require('./package.json');
+
+// root endpoint
+app.get("/", function(req, res){
+	res.send("Hello World!");
+});
+
+// health endpoint
+app.use('/health', healthcheck(
+{
+	healthy: function() {
+		return { Server: 'is up and running'};
+	}
+}));
+
+// metadata endpoint
+app.get("/metadata", function(req, res){
+	var data = {
+		"myapplication" : [
+			{
+				"version": `${metadata.version}`,
+				"description" : `${metadata.description}`
+			}
+		]
+	};
+
+	res.send(data);
+});
+
+app.listen(port, function(){
+	console.log(`The API service started at port ${port}`);
+});
---
+++ test/test-pages.js
@@ -0,0 +1,87 @@
+var expect = require("chai").expect;
+var request = require("request");
+
+// expected data for metadata endpoint
+var metadata = require('../package.json');
+
+// groupd the test into endpoints
+describe('Endpoints:', function() {
+	// the root endpoint
+	describe('Root', function(){
+		 // status
+		it('status', function(done){
+			request('http://localhost:3000', function(error, response, body)
+			{
+				expect(response.statusCode).to.equal(200);
+				done();
+			});
+		});
+
+		// content
+		it('content', function(done){
+			request('http://localhost:3000', function(error, response, body)
+			{
+				expect(body).to.equal("Hello World!");
+				done();
+			});
+		});
+	});
+
+	// the health endpoint
+	describe('Health', function(){
+		// status
+		it('status', function(done){
+			request('http://localhost:3000/health', function(error, response, body)
+			{
+				expect(response.statusCode).to.equal(200);
+				done();
+			});
+		});
+
+		// content
+		it('content', function(done){
+			request('http://localhost:3000/health', function(error, response, body)
+			{
+				const expectedHealthData = { Server: 'is up and running'};
+
+				// compare the two objects
+				expect(JSON.parse(body)).to.deep.equal(expectedHealthData);
+				done();
+			});
+		});
+	});
+
+	// metadata endpoint
+	describe('Metadata', function(){
+		it('status', function(done){
+			request('http://localhost:3000/metadata', function(error, response, body)
+			{
+				expect(response.statusCode).to.equal(200);
+				done();
+			});
+		});
+
+		it('content', function(done){
+			request('http://localhost:3000/metadata', function(error, response, body)
+			{
+				const expectedMetaData = {
+					"myapplication" : [
+					{
+						"version": `${metadata.version}`,
+						"description" : `${metadata.description}`
+					}
+					]
+				};
+
+				// compare the two objects
+				expect(JSON.parse(body)).to.deep.equal(expectedMetaData);
+				done();
+			});
+		});
+	});
+});
+
+
+
+
+
